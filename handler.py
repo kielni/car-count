d533a2fc921ee8abc670109b6113d883c49ff666
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2021-12-14 16:42:33 -0800

    use get since Entry data may be missing

--- handler.py
+++ handler.py
@@ -230,7 +230,7 @@ def update_sheet(values: Dict[str, Dict[str, Any]], now_pt: datetime, write: boo
     mdy = now_pt.strftime("%-m/%-d/%y")
     prefix = {0: "", 1: "A", 2: "B"}  # A-Z, AA-AZ, BA-BZ
     for key in ["EntryA", "EntryB", "ExitA", "ExitB"]:
-        val = max(0, values["entry" if "Entry" in key else "exit"][key])
+        val = max(0, values["entry" if "Entry" in key else "exit"].get(key, 0))
         sheet = ss.get_worksheet(worksheets[key])
         dt_str = sheet.range("A2:A2")[0].value or datetime.now().strftime("%m/%d/%Y")
         latest = date_parser.parse(dt_str).date()
